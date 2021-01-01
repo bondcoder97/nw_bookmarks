aff08574315cf89b6145cdb2719c57152e699b74
bondcoder <andruha3097@gmail.com>
2021-01-01 17:32:44 +0300

    new: bookmarks get, exist

---
+++ bookmarks.js
@@ -0,0 +1,64 @@
+const fs = require('fs').promises;
+
+class Bookmarks{
+    constructor(options={}){
+        const {data_path,filename="bookmarks.txt"} = options;
+        this.data_path = data_path;
+        this.filename = filename;
+        this.bookmark_filepath = `${data_path}/${filename}`;
+    }
+
+
+    async get_bookmarks(){
+        try{
+            const raw_bookmarks = await fs.readFile(this.bookmark_filepath, {encoding: "utf-8"});
+            const bookmarks = this.extract_bookmarks(raw_bookmarks);
+            return bookmarks;
+        }
+        catch(err){
+          console.log(err);
+          return [];
+        }
+
+    }
+
+    async is_exist(url){
+        try{
+            if(!url) throw new Error("Wrong params!");
+            const bookmarks = await this.get_bookmarks();
+            const current_bookmark = bookmarks.find((bookmark) => bookmark === url);
+            if(current_bookmark) return true;
+            return false;
+        }
+        catch(err){
+          console.log(err);
+        }
+    }
+
+
+
+    to_text(bookmarks_list){
+        let result = "";
+        if(!bookmarks_list.length) return result;
+        bookmarks_list.map((bookmark)=>{
+            result += `${bookmark}\n`;
+        });
+        return result;
+    }
+
+    //extract bookmarks info from raw text
+    extract_bookmarks(raw_text){
+        if(!raw_text) return [];
+        const rows = raw_text.split("\n");        
+        const result = [];
+        rows.map((row)=>{
+            if(!row) return;
+            result.push(row);               
+        });
+        return result;
+    }
+    
+}
+
+
+module.exports = Bookmarks;
